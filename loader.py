06e6d7db8d26bff0020f6c40b7141f86e869b1c7
shaaaarky <155846610+shaaaarky@users.noreply.github.com>
2024-03-11 19:27:21 +0100

    reorganizing

--- loader.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Starting CMD and loading main python file
-main_script = "c:/Users/JohnBradshawCantos/Desktop/xwatcher/sysrunn3r.py"
-os.system(f"start cmd /c python {main_script}")
--- x.py
+++
@@ -1,4 +0,0 @@
-import platform
-
-print(platform.node())
-print(platform.system())
